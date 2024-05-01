0b54711fb37a7f123f3ae6902f9054f1d8278a0f
Ronald Castillo <jronaldcll@gmail.com>
2024-04-30 22:12:16 -0500

    feat[ZEGEL]: Initial project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Session 2 - Zegel (Hola Mundo)
         </p>
         <a
           className="App-link"
